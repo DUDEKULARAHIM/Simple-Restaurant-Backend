9f02634b09e354419fa623a02accefedfe5491f3
DUDEKULARAHIM <119217092+DUDEKULARAHIM@users.noreply.github.com>
2024-11-22 21:08:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -18,14 +18,35 @@
 
 
 
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+
+
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    console.log('Connecting to MongoDB with URI:', process.env.MONGO_URI); // Add this line for debugging
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
@@ -34,3 +55,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
